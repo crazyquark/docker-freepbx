f59879bd3ec00abed015f1e05a5f3511044e119e
crazyquark <cristian.sandu@gmail.com>
2020-05-11 10:23:51 +0300

    also create latest tag

--- merge-images.sh
+++ merge-images.sh
@@ -8,3 +8,6 @@ done
 
 docker manifest create crazyquark/freepbx:15 "${arch[@]/#/crazyquark/freepbx:15-}"
 docker manifest push --purge crazyquark/freepbx:15
+
+docker pull crazyquark/freepbx:15
+docker tag crazyquark/freepbx:15 crazyquark/freepbx:latest
